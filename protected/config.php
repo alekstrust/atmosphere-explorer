d54ce0d062e1694423002f68acbf98c40d674046
Javier Távara <javier@tavara.pe>
2015-10-12 23:55:42 -0500

    cambiar fecha de inicio temporal y log path

--- protected/config.php
+++ protected/config.php
@@ -7,7 +7,7 @@ define('DB_PASS', 'windpexplorer');
 define('DB_NAME', 'windpexplorer');
 
 
-defined( 'LOGS_PATH' ) || define( 'LOGS_PATH', '' );
+defined( 'LOGS_PATH' ) || define( 'LOGS_PATH', 'C:\xampp\htdocs\wind-pexplorer\logs\\' );
 
 define( 'FILES_PATH', 'C:\xampp\htdocs\wind-pexplorer\files\\' );
 define( 'NRG_PATH', 'C:\NRG\\' );
@@ -16,4 +16,4 @@ define( 'MIN_ATTACHMENT_SIZE', 6000 );
 
 // tiempo pasado a buscar desde el momento
 // de iniciado el script
-define( 'START_TIME', '14 Dec 2013' );
+define( 'START_TIME', '1 Oct 2015' );

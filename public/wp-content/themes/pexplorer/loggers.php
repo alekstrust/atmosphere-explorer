b4b3b1d1ee9172b68070829d26aaaf8353d59637
Javier Távara <javier@tavara.pe>
2015-10-12 23:56:38 -0500

    personalizar sensor de radiación

--- public/wp-content/themes/pexplorer/loggers.php
+++ public/wp-content/themes/pexplorer/loggers.php
@@ -67,28 +67,7 @@ get_header(); ?>
 
               <?php foreach( $sensors as $sensor ): ?>
 
-                <h5><?php echo $sensor->description; ?> <small>(<?php echo $sensor->units; ?>)</small></h5>
-
-                <p>Canal #<?php echo $sensor->channelNumber; ?></p>
-
-                <?php if( ! empty( $sensor->height ) ): ?>
-                  <small>Altura del sensor: <?php echo $sensor->height; ?>m.</small>
-                <?php endif; ?>
-
-                <?php $records = windpexplorer_get_last_records( $sensor->idSensor ); ?>
-
-                <script type="text/javascript" id="ds-<?php echo $sensor->idSensor; ?>">
-                  var dataSource<?php echo $sensor->idSensor; ?> = [
-                    <?php foreach( $records as $record ): ?>
-                      {
-                        day:   "<?php echo date_format( date_create( $record->dateCreated ), 'd-M' ); ?>",
-                        value: <?php echo $record->avg; ?>
-                      },
-                    <?php endforeach; ?>
-                  ];
-                </script>
-
-                <div id="chart-<?php echo $sensor->idSensor; ?>" class="chart" data-sensor="<?php echo $sensor->idSensor; ?>" data-name="<?php echo $sensor->description; ?> (<?php echo $sensor->units; ?>)" style="height: 300px;"></div>
+                <?php get_template_part( 'sensor', sanitize_title( $sensor->description ) ); ?>
 
               <?php endforeach; ?>
 
---
+++ public/wp-content/themes/pexplorer/sensor-radiacion.php
@@ -0,0 +1,31 @@
+<?php
+
+global $sensor;
+
+$sensor->units = 'kW-h/m^2';
+$sensor->description = 'Radiación solar acumulada';
+
+?>
+
+<h5><?php echo $sensor->description; ?> <small>(<?php echo $sensor->units; ?>)</small></h5>
+
+<p>Canal #<?php echo $sensor->channelNumber; ?></p>
+
+<?php if( ! empty( $sensor->height ) ): ?>
+  <small>Altura del sensor: <?php echo $sensor->height; ?>m.</small>
+<?php endif; ?>
+
+<?php $records = windpexplorer_get_last_records( $sensor->idSensor ); ?>
+
+<script type="text/javascript" id="ds-<?php echo $sensor->idSensor; ?>">
+  var dataSource<?php echo $sensor->idSensor; ?> = [
+    <?php foreach( $records as $record ): ?>
+      {
+        day:   "<?php echo date_format( date_create( $record->dateCreated ), 'd-M' ); ?>",
+        value: <?php echo $record->avg * 0.024; ?>
+      },
+    <?php endforeach; ?>
+  ];
+</script>
+
+<div id="chart-<?php echo $sensor->idSensor; ?>" class="chart" data-sensor="<?php echo $sensor->idSensor; ?>" data-name="<?php echo $sensor->description; ?> (<?php echo $sensor->units; ?>)" style="height: 300px;"></div>
